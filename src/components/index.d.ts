30e9b0f2728fa7587f553806d0a2a5b518b18eef
Federico Rodriguez <federico.rodriguez@wazuh.com>
2021-10-26 11:28:14 +0200

    Added loading chart animation and react-lottie. Changed icon and shadow color

--- src/components/index.d.ts
+++ src/components/index.d.ts
@@ -23,3 +23,8 @@ declare module '@elastic/eui' {
   // @ts-ignore path only exists at build time
   export * from '@elastic/eui/src/components/date_picker/react-datepicker'; // eslint-disable-line import/no-unresolved
 }
+
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
