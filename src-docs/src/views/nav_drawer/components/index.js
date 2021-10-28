f647ca60eb992542edce8d4b785998dcd5b527ba
Federico Rodriguez <federico.rodriguez@wazuh.com>
2021-10-28 13:31:06 +0200

    Refactored navdrawer example in separate components

--- src-docs/src/views/nav_drawer/components/index.js
+++ src-docs/src/views/nav_drawer/components/index.js
@@ -2,5 +2,7 @@ import MultiSelect from './multi_select';
 import DatePicker from './datepicker';
 import Searchbar from './searchbar';
 import Notice from './notice';
+import Headers from './headers';
+import MainBody from './main_body';
 
-export { MultiSelect, DatePicker, Searchbar, Notice };
+export { MultiSelect, DatePicker, Searchbar, Notice, Headers, MainBody };
